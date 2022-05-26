f1e72bc7bbc548b7b4d6f1a2c15d74c558821108
nine <steve.chasse@gmail.com>
2022-05-26 01:36:46 -0400

    updated script header with references/sources

---
+++ releases/sccsr15forms-threadeddeferredrender/sccsr15forms/tutorialcube.cs
@@ -0,0 +1,272 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////
+//DEVELOPED BY STEVE CHASSÉ using xoofx's sharpdx original deferred rendering sample.            //
+//mix of rastertek c# github user dan6040's sample and xoofx samples and smartrak's architecture.//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Copyright (c) 2010-2013 SharpDX - Alexandre Mutel 
+//  
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions: 
+//  
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software. 
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+// THE SOFTWARE. 
+
+
+//The MIT License (MIT)
+//
+//Copyright(c) 2016 Smartrak
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+
+//https://github.com/Dan6040/SharpDX-Rastertek-Tutorials
+//https://github.com/Smartrak/WpfSharpDxControl
+//https://github.com/sharpdx/SharpDX-Samples
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.Direct2D1;
+using SharpDX.DXGI;
+
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D;
+using SharpDX.Windows;
+using Buffer = SharpDX.Direct3D11.Buffer;
+using Color = SharpDX.Color;
+using Device = SharpDX.Direct3D11.Device;
+using MapFlags = SharpDX.Direct3D11.MapFlags;
+
+using InputElement = SharpDX.Direct3D11.InputElement;
+
+
+namespace sccsr15forms
+{
+    internal class tutorialcube : IDisposable
+    {
+        public Buffer staticContantBuffer;
+        public Buffer dynamicConstantBuffer;
+        public InputLayout layout;
+        public PixelShader pixelShader;
+        public VertexShader vertexShader;
+        public Buffer verticesbuffer;
+        public Vector4[] vertices;
+
+        // Create Constant Buffer 
+        /*public Buffer staticContantBufferuithread;
+        public Buffer dynamicConstantBufferuithread;
+
+        // Create Constant Buffer 
+        public Buffer staticContantBuffersysthread;//
+        public Buffer dynamicConstantBuffersysthread;*/
+
+
+        public tutorialcube(Device device)
+        {
+            // Compile Vertex and Pixel shaders 
+            var bytecode = ShaderBytecode.CompileFromFile("MultiCube.fx", "VS", "vs_4_0");
+            vertexShader = new VertexShader(device, bytecode);
+
+            // Layout from VertexShader input signature 
+            layout = new InputLayout(device, ShaderSignature.GetInputSignature(bytecode), new[]
+                   {
+                        new InputElement("POSITION", 0, Format.R32G32B32A32_Float, 0, 0),
+                        new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 16, 0)
+                    });
+            bytecode.Dispose();
+
+            bytecode = ShaderBytecode.CompileFromFile("MultiCube.fx", "PS", "ps_4_0");
+            pixelShader = new PixelShader(device, bytecode);
+            bytecode.Dispose();
+
+
+            vertices = new Vector4[]
+            {
+                new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f), // Front 
+                new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+
+                new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f), // BACK 
+                new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+
+                new Vector4(-1.0f, 1.0f, -1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f), // Top 
+                new Vector4(-1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4(-1.0f, 1.0f, -1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, 1.0f, -1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+
+                new Vector4(-1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), // Bottom 
+                new Vector4( 1.0f,-1.0f,  1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4(-1.0f,-1.0f,  1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4(-1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,-1.0f,  1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+
+                new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), // Left 
+                new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+
+                new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f), // Right 
+                new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+            };
+
+
+            // Instantiate Vertex buiffer from vertex data 
+            verticesbuffer = Buffer.Create(device, BindFlags.VertexBuffer, vertices);
+
+            // Create Constant Buffer 
+            staticContantBuffer = new Buffer(device, Utilities.SizeOf<Matrix>(), ResourceUsage.Default, BindFlags.ConstantBuffer, CpuAccessFlags.None, ResourceOptionFlags.None, 0);
+            dynamicConstantBuffer = new Buffer(device, Utilities.SizeOf<Matrix>(), ResourceUsage.Dynamic, BindFlags.ConstantBuffer, CpuAccessFlags.Write, ResourceOptionFlags.None, 0);
+
+            // Create Constant Buffer 
+            /* staticContantBufferuithread = new Buffer(device, Utilities.SizeOf<Matrix>(), ResourceUsage.Default, BindFlags.ConstantBuffer, CpuAccessFlags.None, ResourceOptionFlags.None, 0);
+             dynamicConstantBufferuithread = new Buffer(device, Utilities.SizeOf<Matrix>(), ResourceUsage.Dynamic, BindFlags.ConstantBuffer, CpuAccessFlags.Write, ResourceOptionFlags.None, 0);
+
+             // Create Constant Buffer 
+             staticContantBuffersysthread = new Buffer(device, Utilities.SizeOf<Matrix>(), ResourceUsage.Default, BindFlags.ConstantBuffer, CpuAccessFlags.None, ResourceOptionFlags.None, 0);
+             dynamicConstantBuffersysthread = new Buffer(device, Utilities.SizeOf<Matrix>(), ResourceUsage.Dynamic, BindFlags.ConstantBuffer, CpuAccessFlags.Write, ResourceOptionFlags.None, 0);
+            */
+
+
+        }
+
+
+        ~tutorialcube()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this); // so that Dispose(false) isn't called later
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (staticContantBuffer != null)
+                {
+                    staticContantBuffer.Dispose();
+                    staticContantBuffer = null;
+                }
+                if (dynamicConstantBuffer != null)
+                {
+                    dynamicConstantBuffer.Dispose();
+                    dynamicConstantBuffer = null;
+                }
+
+                /*if (staticContantBufferuithread != null)
+                {
+                    staticContantBufferuithread.Dispose();
+                    staticContantBufferuithread = null;
+                }
+                if (dynamicConstantBufferuithread != null)
+                {
+                    dynamicConstantBufferuithread.Dispose();
+                    dynamicConstantBufferuithread = null;
+                }
+                if (staticContantBuffersysthread != null)
+                {
+                    staticContantBuffersysthread.Dispose();
+                    staticContantBuffersysthread = null;
+                }
+                if (dynamicConstantBuffersysthread != null)
+                {
+                    dynamicConstantBuffersysthread.Dispose();
+                    dynamicConstantBuffersysthread = null;
+                }*/
+
+                if (layout != null)
+                {
+                    layout.Dispose();
+                    layout = null;
+                }
+                if (pixelShader != null)
+                {
+                    pixelShader.Dispose();
+                    pixelShader = null;
+                }
+
+                if (vertexShader != null)
+                {
+                    vertexShader.Dispose();
+                    vertexShader = null;
+                }
+
+                if (verticesbuffer != null)
+                {
+                    verticesbuffer.Dispose();
+                    verticesbuffer = null;
+                }
+
+                if (vertices != null)
+                {
+                    vertices = null;
+                }
+
+
+
+
+
+
+                // Dispose all owned managed objects
+            }
+
+            // Release unmanaged resources
+        }
+    }
+}
---
+++ releases/sccsr15forms-threadeddeferredrender/sccsr15forms/updatePrim.cs
@@ -0,0 +1,165 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////
+//DEVELOPED BY STEVE CHASSÉ using xoofx's sharpdx original deferred rendering sample.            //
+//mix of rastertek c# github user dan6040's sample and xoofx samples and smartrak's architecture.//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Copyright (c) 2010-2013 SharpDX - Alexandre Mutel 
+//  
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions: 
+//  
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software. 
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+// THE SOFTWARE. 
+
+
+//The MIT License (MIT)
+//
+//Copyright(c) 2016 Smartrak
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+
+//https://github.com/Dan6040/SharpDX-Rastertek-Tutorials
+//https://github.com/Smartrak/WpfSharpDxControl
+//https://github.com/sharpdx/SharpDX-Samples
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using System.Diagnostics;
+
+
+namespace sccsr15forms
+{
+    internal class updatePrim : IDisposable
+    {
+        public static updatePrim currentupdatePrim;
+        public directx D3D;
+        public Camera camera;
+
+ 
+
+
+        public updatePrim(directx D3D_)
+        {
+            currentupdatePrim = this;
+            D3D = D3D_;
+
+            camera = new Camera();
+
+            camera.SetPosition(0,0,-1);
+            camera.SetRotation(0, 180, 0);
+
+            Console.WriteLine("created updatePrim");
+        }
+
+        //TEST FOR UI THREAD VS SYSTEM.THREAD PERFORMANCE. SYSTEM.THREAD IS FASTER.
+        /*public int counteruithread;
+        public int countersystemthread;
+
+        public void updatescriptsprimUIThread()
+        {
+            counteruithread++;
+        }
+        public void updatescriptsprimSystemThread()
+        {
+            countersystemthread++;
+        }*/
+
+        public void updatescriptsprimstartrender()
+        {
+            //startrender();
+            //camera.Render();
+
+        }
+        public void updatescriptsprimstoprender()
+        {
+          
+            stoprender();
+        }
+
+
+
+
+        public void startrender()
+        {
+            if (D3D != null)
+            {
+                if (D3D.DepthStencilView != null)
+                {
+                    D3D.DeviceContext.ClearDepthStencilView(D3D.DepthStencilView, SharpDX.Direct3D11.DepthStencilClearFlags.Depth, 1.0f, 0); //new SharpDX.Color(255, 15, 15, 255)
+
+                }
+                if (D3D.RenderTargetView != null)
+                {
+                    D3D.DeviceContext.ClearRenderTargetView(D3D.RenderTargetView, SharpDX.Color.CornflowerBlue); //SharpDX.Color.LightGray //Black //new SharpDX.Color(255, 15,
+
+                }
+            }
+        }
+
+        public void stoprender()
+        {
+            if (D3D != null)
+            {
+                if (D3D.SwapChain != null)
+                {
+                    D3D.SwapChain.Present(0, SharpDX.DXGI.PresentFlags.None);
+                }
+            }
+        }
+
+
+        ~updatePrim()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this); // so that Dispose(false) isn't called later
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Dispose all owned managed objects
+            }
+
+            // Release unmanaged resources
+        }      
+    }
+}
